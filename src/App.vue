3493f0770f325a54ca98d7db929f2564550990b0
Will-iam210 <axeso_william@hotmail.com>
2021-09-28 17:55:24 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Chat v-if="user"></Chat>
+    <Login v-else></Login>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Chat from './components/Chat'
+import Login from './components/Login'
+import firebase from "firebase/compat";
 export default {
+  data() {
+    return {
+      user: firebase.auth().currentUser
+    }
+  },
   name: 'App',
   components: {
-    HelloWorld
+    Chat, Login
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #F7F8F3;
 }
 </style>
